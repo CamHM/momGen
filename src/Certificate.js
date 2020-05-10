d67b479d5e5a25298de17d7803a4f18fb0db75c4
Camila Andrea Herrera Melo <cami.andre469@gmail.com>
2020-05-10 07:28:19 -0500

    actualizacion certificado

--- src/Certificate.js
+++ src/Certificate.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from './assets/icon.jpeg';
 import moment from "moment";
 import 'moment/locale/es';
+import './Certificate.css';
 
 moment.locale('es');
 
@@ -11,41 +12,48 @@ const Certificate = ({ img, name }) => {
         container: {
             display: 'flex',
             alignItems: 'center',
+            height: '100vh',
         },
         body: {
-            border: 'dashed 1px lightgray',
             margin: '15px',
-            padding: '20px',
+            padding: '90px 100px 0 100px',
+            height: '100%',
         },
         name: {
-            textDecoration: 'underline',
+            fontSize: '2rem',
+            borderBottom: '#fe8395 1px solid',
+            color: '#fe8395',
         },
     };
 
     return (
         <div style={style.container} className="hero is-fullheight">
-            <div style={style.body}>
+            <div style={style.body} className="frame">
                 <div className="columns has-text-centered">
-                    <div className="column is-half">
-                        <p className="title is-4">
+                    <div className="column is-4 firstCol">
+                        <p className="title is-4 text">
                             Felíz día
                         </p>
-                        <p className="subtitle is-6">
+                        <p className="subtitle is-6 text">
                             { moment(new Date(Date.now())).format('LL') }
                         </p>
-                        <img className="is-rounded" src={logo} alt="NODO" width="30%"/>
-                        <br/><br/>
-                        <p className="subtitle is-5 is-spaced">
-                            ¡Nos la estamos jugando toda!
-                        </p>
+                        <div className="mainLogo">
+                            <img className="is-rounded" src={logo} alt="NODO" width="50%"/>
+                            <br/><br/>
+                            <p className="subtitle is-6 is-spaced">
+                                ¡Nos la estamos jugando toda!
+                            </p>
+                        </div>
                     </div>
-                    <div className="column">
-                        <img src={img} alt="imagen" width="50%"/>
+                    <div className="column is-7">
+                        <img className="picture" src={img} alt="imagen" width="70%"/>
                         <br/><br/>
-                        <p className="title is-4 is-spaced">Concedido a: <span style={style.name}>{ name }</span></p>
-                        <p className="subtitle">
-                            Por todos sus méritos habidos y por haber, que son muchos e insuperables
-                        </p>
+                        <p className="title is-6 is-spaced">Concedido a: <span style={style.name}>{ name }</span></p>
+                        <div className="description">
+                            <p className="subtitle is-6">
+                                Por todos sus méritos habidos y por haber, que son muchos e insuperables.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
